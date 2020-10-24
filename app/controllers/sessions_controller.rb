caa396a0dfaca9f577fe8447e58e53c899e0de27
Dasha Kondratyeva <dashakondratieva@MacBook-Pro-7.fios-router.home>
2020-10-24 15:26:59 -0400

    moved helpers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,17 +23,4 @@ class SessionsController < ApplicationController
         redirect '/login'
     end
 
-
-    helpers do
-        def logged_in?
-          !!session[:user_id]
-        end
-    
-        def current_user
-          User.find(session[:user_id])
-        end
-    
-      end
-
-
 end
